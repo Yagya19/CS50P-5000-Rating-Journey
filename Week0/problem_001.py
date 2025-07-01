e45fd27112b03745ff272abf691364d1d2e09f31
Yagya19 <yagyadeepsharma19@gmail.com>
2025-07-01 20:50:01 +0100

    Problem_001.py
    Problem 1/1000 : Hello World function - Added professional comments (Rating: 950)

---
+++ Week0/problem_001.py
@@ -0,0 +1,8 @@
+# Create a function that prints a text 
+def main():  # Define a main function + use : 
+    say_hello()   # Indent + Call the function 
+
+def say_hello():  # Define the function + : 
+    print("Hello,World!") # Print job the function 
+
+main()  # Call the main function 
